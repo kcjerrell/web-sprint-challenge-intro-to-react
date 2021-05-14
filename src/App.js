156f5b448e46fae5285ec1ab111c6d040f418062
Kelly Jerrell <kcjerrell@gmail.com>
2021-05-14 01:11:47 -0700

    bare minimum mvp met

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Character from './components/Character';
+import { API_ENDPOINT } from "./constants";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+
+    const fetchChars = async () => {
+      try {
+        const response = await axios.get(API_ENDPOINT);
+        setCharacters(response.data);
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+    }
+
+    fetchChars();
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(char => (
+        <Character data={char} key={char.url} />
+      ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
 // Write your Character component here
+
+const StyledComp = styled.div`
+
+`;
+
+const Character = (props) => {
+	const { name } = props.data;
+
+	return (
+		<StyledComp>
+			{name}
+	</StyledComp>
+);
+};
+
+export default Character;
